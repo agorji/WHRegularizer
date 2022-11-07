a6c43285b9f031b47a622a197faac39904154969
Ali Gorji <alli.gorji@gmail.com>
2022-11-07 21:56:51 +0100

    Add spectrum convergence notebook

--- utils.py
+++ utils.py
@@ -1,10 +1,11 @@
+import matplotlib.pyplot as plt
 import numpy as np
-from sklearn.model_selection import train_test_split
-from sklearn.metrics import r2_score, mean_squared_error
+import seaborn as sns
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
 from torch.utils.data import Dataset, DataLoader, random_split
+from torch.optim.lr_scheduler import StepLR
 from typing import Dict, List
 
 from itertools import product
@@ -55,14 +56,25 @@ class FourierDataset(Dataset):
         return (torch.rand(self.k, self.n) < p).float()
 
     def uniform_deg_freq(self, d):
-        freqs = torch.zeros(self.k, self.n)
-        one_rows, one_cols = [], []
+        freqs = []
         for i in range(self.k):
             deg = random.randint(1, d)
-            one_rows.extend([i]*deg)
-            one_cols.extend(random.sample(range(self.n), deg))
-        freqs[one_rows, one_cols] = 1.0
-        return freqs
+            is_duplicate = True
+            while is_duplicate:
+                one_indices = random.sample(range(self.n), deg)
+                new_f = [1.0 if i in one_indices else 0.0 for i in range(self.n)]
+                if new_f not in freqs:
+                    freqs.append(new_f)
+                    is_duplicate = False
+        return torch.tensor(freqs)
+
+    def get_int_freqs(self):
+        return self.freq_f.cpu().numpy().dot(2**np.arange(self.n)[::-1]).astype(int)
+
+    def get_fourier_spectrum(self):
+        spectrum = np.zeros(2**self.n)
+        spectrum[self.get_int_freqs()] = self.amp_f.cpu().numpy()
+        return spectrum
 
 
 def hadamard_matrix(n, normalize=False):
@@ -86,8 +98,8 @@ def get_sample_inputs(n, b):
 
 
 class ModelTrainer:
-    def __init__(self, model: nn.Module, dataset: Dataset, config: Dict, p_test = 0.25, 
-                    device = "cuda", log_wandb = False):
+    def __init__(self, model: nn.Module, dataset: Dataset, config: Dict, p_val = 0.25, device = "cuda", 
+                    log_wandb = False, report_epoch_fourier=False, print_logs=True, plot_results=False):
         '''
             Trains a torch model given the dataset and the training method.
 
@@ -99,6 +111,9 @@ class ModelTrainer:
                 - config: a config dictionary that includes all the hyperparameters for training
                 - p_test: relative size of the validation dataset
                 - device: torch device used for the training
+                - log_wandb: sets whether log results through wandb
+                - report_epoch_fourier: sets if the exact Fourier transform of the model should be computed
+                                         and reported.
         '''
 
         self.device = device
@@ -106,10 +121,15 @@ class ModelTrainer:
         self.config = config
         self.log_wandb = log_wandb
         self.dataset = dataset
+        self.report_epoch_fourier = report_epoch_fourier
+        self.logs = []
+        self.plot_results = plot_results
+        self.print_logs = print_logs
 
         # Dataloader
-        test_size = int(p_test * len(self.dataset))
-        train_ds, val_ds = random_split(self.dataset, lengths=[len(self.dataset) - test_size, test_size])
+        self.val_size = int(p_val * len(self.dataset))
+        self.train_size = len(self.dataset) - self.val_size
+        train_ds, val_ds = random_split(self.dataset, lengths=[self.train_size, self.val_size])
 
         self.train_loader = DataLoader(train_ds, batch_size=config["batch_size"], shuffle=True)
         self.val_loader = DataLoader(val_ds, batch_size=config["batch_size"], shuffle=True)
@@ -150,67 +170,114 @@ class ModelTrainer:
 
         # Training stuff
         self.optim = torch.optim.Adam(model.parameters(), lr=config["lr"], weight_decay=config["weight_decay"])
+        self.scheduler = StepLR(self.optim, step_size=config.get("scheduler_step_size", 20), gamma=config.get("scheduler_gamma", 1))
+
+        # Fourier of network
+        if self.report_epoch_fourier:
+            self.original_H = hadamard_matrix(self.n, normalize=True).to(device)
+
+        # Calculating R2
+        train_mean_y = sum([torch.sum(y).item() for _, y in self.train_loader]) / len(train_ds)
+        val_mean_y = sum([torch.sum(y).item() for _, y in self.val_loader]) / len(val_ds)
+        self.train_tss = sum([torch.sum((y-train_mean_y)**2).item() for _, y in self.train_loader])
+        self.val_tss = sum([torch.sum((y-val_mean_y)**2).item() for _, y in self.val_loader])
 
     def train_model(self):
         device = self.device
-
+        spectrums = []
         self.model.to(device)
+
+        # Compute the Fourier spectrum of Model before training
+        if self.report_epoch_fourier:
+            self.model.eval()
+            with torch.no_grad():
+                all_inputs = torch.asarray(list((product((0.0,1.0), repeat=self.n))))
+                landscape = self.model(all_inputs.to(device))
+                fourier_spectrum = self.original_H @ landscape
+                spectrums.append(fourier_spectrum.cpu().numpy())
+
         for epoch in range(self.config["num_epochs"]):
             # Train epoch based on the set training method
             self.model.train()
-            y_pred, y_true, epoch_log = self.train_epoch()
-            epoch_log["train_mse_loss"] = mean_squared_error(y_true, y_pred)
-            epoch_log["train_r2"] = r2_score(y_true, y_pred)
+            RSS, epoch_log = self.train_epoch()
+            epoch_log["train_mse_loss"] = RSS / self.train_size
+            epoch_log["train_r2"] = 1 - RSS / self.train_tss
 
             # Evaluate the model on validation set
             self.model.eval()
-            y_true, y_pred = [], []
             with torch.no_grad():
+                RSS = 0
                 for X, y in self.val_loader:
                     X, y = X.to(device), y.to(device)
                     y_hat = self.model(X)
-                    y_true.extend(y.tolist())
-                    y_pred.extend(y_hat.tolist())
+                    RSS += torch.sum((y_hat-y)**2).item()
+                
+                # Compute the Fourier spectrum of Model if required
+                if self.report_epoch_fourier:
+                    all_inputs = torch.asarray(list((product((0.0,1.0), repeat=self.n))))
+                    landscape = self.model(all_inputs.to(device))
+                    fourier_spectrum = self.original_H @ landscape
+                    spectrums.append(fourier_spectrum.cpu().numpy())
             
-            epoch_log["val_mse_loss"] = mean_squared_error(y_true, y_pred)
-            epoch_log["val_r2"] = r2_score(y_true, y_pred)
-            print(f"#{epoch} - Train Loss: {epoch_log['train_mse_loss']:.3f}, R2: {epoch_log['train_r2']:.3f}"\
-                f"\tValidation Loss: {epoch_log['val_mse_loss']:.3f}, R2: {epoch_log['val_r2']:.3f}")
+            epoch_log["val_mse_loss"] = RSS / self.val_size
+            epoch_log["val_r2"] = 1 - RSS / self.val_tss
+            if self.print_logs:
+                print(f"#{epoch} - Train Loss: {epoch_log['train_mse_loss']:.3f}, R2: {epoch_log['train_r2']:.3f}"\
+                    f"\tValidation Loss: {epoch_log['val_mse_loss']:.3f}, R2: {epoch_log['val_r2']:.3f}")
 
             # Log wandb
+            self.logs.append(epoch_log)
             if self.log_wandb:
                 wandb.log(epoch_log)
+            
+            self.scheduler.step()
+        
+        if self.plot_results:
+            self.plot_logs()
+            
+        if self.report_epoch_fourier:
+            return spectrums
+    
+    def plot_logs(self):
+        loss_data = {"train_mse_loss": [l["train_mse_loss"] for l in self.logs],
+                    "val_mse_loss": [l["val_mse_loss"] for l in self.logs]}
+        r2_data = {"train_r2": [l["train_r2"] for l in self.logs],
+                    "val_r2": [l["val_r2"] for l in self.logs]}
+
+        fig, axes = plt.subplots(1, 2, figsize=(8, 3), sharex=True)
+        fig.suptitle(self.config["training_method"])
+
+        sns.lineplot(loss_data, ax=axes[0])
+        sns.lineplot(r2_data, ax=axes[1])
+        axes[1].set_ylim(0, 1)
     
     def normal_epoch(self):
         device = self.device
-        outputs, ground_truth = [], []
+        RSS = 0
         batch_train_loss = []
         for X, y in self.train_loader:
             self.optim.zero_grad()
             X, y = X.to(device), y.to(device)
             y_hat = self.model(X)
             loss = F.mse_loss(y, y_hat)
-            batch_train_loss.append(loss)
-            outputs.extend(y_hat.tolist())
-            ground_truth.extend(y.tolist())
+            RSS += torch.sum((y_hat-y)**2).item()
 
             loss.backward()
             self.optim.step()
         
-        return outputs, ground_truth, {}
+        return RSS, {}
     
     def hashing_epoch(self):
         device = self.device
         batch_train_loss, batch_hadamard_loss = [], []
-        outputs, ground_truth = [], []
+        RSS = 0
         for X, y in self.train_loader:
             self.optim.zero_grad()
             X, y = X.to(device), y.to(device)
             y_hat = self.model(X)
             loss = F.mse_loss(y, y_hat)
             batch_train_loss.append(loss)
-            outputs.extend(y_hat.tolist())
-            ground_truth.extend(y.tolist())
+            RSS += torch.sum((y_hat-y)**2).item()
 
             # Find the sample inputs using the hashing scheme
             sample_inputs = get_sample_inputs(self.n, self.config["b"])
@@ -227,22 +294,21 @@ class ModelTrainer:
             loss.backward()
             self.optim.step()
         
-        return outputs, ground_truth, {
+        return RSS, {
             'hashing_loss': torch.mean(torch.stack(batch_hadamard_loss)).item(),
         }
     
     def EN_epoch(self):
         device = self.device
         batch_train_loss, batch_hadamard_loss = [], []
-        outputs, ground_truth = [], []
+        RSS = 0
         for X, y in self.train_loader:
             self.optim.zero_grad()
             X, y = X.to(device), y.to(device)
             y_hat = self.model(X)
             loss = F.mse_loss(y, y_hat)
             batch_train_loss.append(loss)
-            outputs.extend(y_hat.tolist())
-            ground_truth.extend(y.tolist())
+            RSS += torch.sum((y_hat-y)**2).item()
 
             # Compute the Hadamard transform of all possible inputs and add to loss
             landscape = self.model(self.all_inputs).reshape(-1)
@@ -255,7 +321,7 @@ class ModelTrainer:
             loss.backward()
             self.optim.step()
         
-        return outputs, ground_truth, {
+        return RSS, {
             'EN_loss': torch.mean(torch.stack(batch_hadamard_loss)).item(),
         }
     
@@ -263,15 +329,14 @@ class ModelTrainer:
         device = self.device
         l2_loss = nn.MSELoss()
         batch_train_loss, batch_hadamard_loss = [], []
-        outputs, ground_truth = [], []
+        RSS = 0
         for X, y in self.train_loader:
             self.optim.zero_grad()
             X, y = X.to(device), y.to(device)
             y_hat = self.model(X)
             loss = F.mse_loss(y, y_hat)
             batch_train_loss.append(loss)
-            outputs.extend(y_hat.tolist())
-            ground_truth.extend(y.tolist())
+            RSS += torch.sum((y_hat-y)**2).item()
 
             wht_out = self.model(torch.tensor(self.X_all, dtype=torch.float, device=device)).reshape(-1)
             wht_diff = wht_out - torch.tensor(self.Hu, dtype=torch.float, device=device) + torch.tensor(self.lam, dtype=torch.float, device=device)
@@ -300,6 +365,6 @@ class ModelTrainer:
             # update the dual 
             self.lam = self.lam + y_hat_all - self.Hu
         
-        return outputs, ground_truth, {
+        return RSS, {
             'ENS_loss': torch.mean(torch.stack(batch_hadamard_loss)).item(),
         }
